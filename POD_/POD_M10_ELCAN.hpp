05ad895567e141569c05cf94407b684018de28b5
Nebelras <Nebelras@gmail.com>
2021-02-14 00:47:31 +0100

    Added Weapon to POD_S_Medic
    Added Weapon to POD_S_Medic

---
+++ POD_/POD_M10_ELCAN.hpp
@@ -0,0 +1,13 @@
+	class gtr_weap_L119A2_CBN_Tan
+	class POD_gtr_weap_L119A2_CBN_Tan: gtr_weap_L119A2_CBN_Tan
+	{
+		author = "[POD Nebel]";
+		_generalMacro = "POD_gtr_weap_L119A2_CBN_Tan";
+		class LinkedItems
+		{
+			class LinkedItemsOptic
+			{
+				slot = "CowsSlot";
+				item = "optic_ElcanC79";
+			};
+		};
--- POD_/config.cpp
+++ POD_/config.cpp
@@ -34,7 +34,7 @@ class CfgPatches
 		author = "Platoon of Danes - Mikkelsen";
 		name = "Platoon of Danes Boxes";
 		url = "https://www.platoonofdanes.com";
-		requiredAddons[] = {"A3_Data_F","A3_Structures_F","ace_medical_treatment","A3_Supplies_F_Heli_CargoNets","A3_Characters_F"};
+		requiredAddons[] = {"A3_Data_F","A3_Structures_F","ace_medical_treatment","A3_Supplies_F_Heli_CargoNets","A3_Characters_F","L119A2"};
 		requiredVersion = 0.1;
 		units[] = {"POD_Boxes_Medic","POD_Boxes_Banana","POD_S_Medic"};
 		weapons[] = {};
@@ -66,7 +66,9 @@ class CfgEditorSubcategories
 	};
 };
 
-
+class cfgWeapons{
+	#include "POD_M10_ELCAN.hpp"
+};
 
 
 class cfgVehicles

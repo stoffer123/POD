9db50ebd159496a5a84a2eaeea06ff9fad97cdf3
Nebelras <Nebelras@gmail.com>
2021-02-15 00:30:43 +0100

    Revert "Update POD_Soldat.hpp"
    This reverts commit 3e5e6e156e4afdf39f485393b9daf560e7cbbc00.

--- POD_/POD_Soldat.hpp
+++ POD_/POD_Soldat.hpp
@@ -69,7 +69,7 @@
 
 
 	// LMG 					
-	class POD_LMG_Soldat: POD_Soldat	
+		class POD_LMG_Soldat: POD_Soldat	
 	{
 		author = "[POD] Nebel";					
 		weapons[] = {POD_LMG_Elcan_LYLA, Throw, Put};			
@@ -108,4 +108,6 @@
 			mag_2(Chemlight_green),
 			mag_2(HandGrenade)
 			};
+	};
+
 	};

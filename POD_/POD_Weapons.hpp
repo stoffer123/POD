a539d4510b127a792f461b9508b6757b71c58385
Nebelras <Nebelras@gmail.com>
2021-02-15 00:10:44 +0100

    Update POD_Weapons.hpp

--- POD_/POD_Weapons.hpp
+++ POD_/POD_Weapons.hpp
@@ -5,6 +5,7 @@
 		displayName="POD M10";
 		scope=2;
 		scopeCurator=2;
+		scopeArsenal = 2;
 		_generalMacro = "POD_M10_Elcan_LYLA";
 		magazines[] = {"30Rnd_556x45_Stanag"};
 		class LinkedItems
@@ -28,6 +29,7 @@
 		displayName="POD M60";
 		scope=2;
 		scopeCurator=2;
+		scopeArsenal = 2;
 		_generalMacro = "POD_LMG_Elcan_LYLA";
 		magazines[] = {"100Rnd_762x51_Mag"};
 		class LinkedItems

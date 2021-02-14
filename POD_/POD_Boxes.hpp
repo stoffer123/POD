111542b1ecf7168703a4179ccd6591725181fb95
stoffer123 <christophermikkelsen@live.dk>
2021-02-14 16:29:30 +0100

    Added POD_Boxes_HGR

--- POD_/POD_Boxes.hpp
+++ POD_/POD_Boxes.hpp
@@ -63,3 +63,34 @@ class B_CargoNet_01_ammo_F;
 		//MACRO_ADDBACKPACK(BACKPACK,COUNT)
 		{};
 	};
+	
+	class POD_Boxes_HGR: B_CargoNet_01_ammo_F
+	{
+		scope = 2;		//
+		displayName = "POD HGR Kasse";
+		editorCategory = "POD_EdCat";
+		editorSubcategory = "POD_EdSubcat_Boxes";
+		transportMaxItems = 5000;
+		
+		class TransportWeapons
+		//MACRO_ADDWEAPON(WEAPON,COUNT)
+		{};
+		
+		class TransportMagazines
+		//MACRO_ADDMAG(MAGAZINE,COUNT)
+		{
+			MACRO_ADDMAG(HandGrenade,50)
+			MACRO_ADDMAG(SmokeShell,50)
+			MACRO_ADDMAG(ACE_M84,50)
+			MACRO_ADDMAG(rhs_mag_m69,50)
+			
+		};
+		
+		class TransportItems
+		//MACRO_ADDITEM(ITEM,COUNT)
+		{};
+		
+		class TransportBackpacks
+		//MACRO_ADDBACKPACK(BACKPACK,COUNT)
+		{};
+	};

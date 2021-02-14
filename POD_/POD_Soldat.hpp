99be16bbccf22567d3176d046332624dcbb70772
Nebelras <Nebelras@gmail.com>
2021-02-15 00:03:34 +0100

    Test soldat removed

--- POD_/POD_Soldat.hpp
+++ POD_/POD_Soldat.hpp
@@ -69,80 +69,12 @@
 
 
 	// LMG 					
-	class POD_LMG_Soldat: POD_Soldat	
-	{
-		author = "[POD] Nebel";		
-		scope = 2;						
-		scopeCurator = 2;				
-		scopeArsenal = 2;				
-		identityTypes[] =  {"LanguageENG_F","Head_NATO","G_NATO_default"};	
-		weapons[] = {POD_LMG_Elcan_LYLA, Throw, Put};			
-		respawnWeapons[] = {POD_LMG_Elcan_LYLA, Throw, Put};		
-		displayName = "Soldat LMG";
-		editorCategory = "POD_EdCat";
-		editorSubcategory = "POD_EdSubcat_Soldiers";
-		backpack = "B_Carryall_mcamo";
-		Items[] = {
-			mag_2(ACE_elasticBandage),
-			mag_2(ACE_CableTie),
-			mag_2(ACE_morphine),
-			mag_2(ACE_tourniquet),
-			ACE_SpareBarrel,
-			ACE_PRC343
-		};			
-
-		RespawnItems[] = {
-			mag_2(ACE_elasticBandage),
-			mag_2(ACE_CableTie),
-			mag_2(ACE_morphine),
-			mag_2(ACE_tourniquet),
-			ACE_SpareBarrel,
-			ACE_PRC343
-		};					
-
-		magazines[] = {
-			mag_8(100Rnd_762x51_Mag), 
-			SmokeShell, SmokeShellGreen, 
-			mag_2(Chemlight_green),
-			mag_2(HandGrenade)
-			};
-
-		respawnMagazines[] = {
-			mag_8(100Rnd_762x51_Mag),
-			SmokeShell, SmokeShellGreen,
-			mag_2(Chemlight_green),
-			mag_2(HandGrenade)
-			};
-
-		linkedItems[] = {
-			V_DDAM_Vest_MTS_TYR,
-			H_DDAM_Helmet_M11_camo,
-			ItemMap,
-			ItemCompass,
-			ItemWatch,
-			ItemRadio,
-			rhs_googles_clear,
-			DDAM_PVS14
-			};
-
-		respawnLinkedItems[] = {
-			V_DDAM_Vest_MTS_TYR,
-			H_DDAM_Helmet_M11_camo,
-			ItemMap,
-			ItemCompass,
-			ItemWatch,
-			ItemRadio,
-			rhs_googles_clear,
-			DDAM_PVS14
-			};
-	};
-
-		class POD_LMG_Soldat_1: POD_Soldat	
+		class POD_LMG_Soldat: POD_Soldat	
 	{
 		author = "[POD] Nebel";					
 		weapons[] = {POD_LMG_Elcan_LYLA, Throw, Put};			
 		respawnWeapons[] = {POD_LMG_Elcan_LYLA, Throw, Put};		
-		displayName = "Soldat LMG TEST";
+		displayName = "Soldat LMG";
 		editorCategory = "POD_EdCat";
 		editorSubcategory = "POD_EdSubcat_Soldiers";
 		Items[] = {
--- POD_/config.cpp
+++ POD_/config.cpp
@@ -34,7 +34,7 @@ class CfgPatches
 		url = "https://www.platoonofdanes.com";
 		requiredAddons[] = {"A3_Data_F","A3_Structures_F","ace_medical_treatment","A3_Supplies_F_Heli_CargoNets","A3_Characters_F","L119A2"};
 		requiredVersion = 0.1;
-		units[] = {"POD_Boxes_Medic","POD_Boxes_Banana","POD_Soldat","POD_Soldat_LMG","POD_Soldat_LMG1"};
+		units[] = {"POD_Boxes_Medic","POD_Boxes_Banana","POD_Soldat","POD_Soldat_LMG"};
 		weapons[] = {"POD_M10_Elcan_LYLA","POD_LMG_Elcan_LYLA"};
 	};
 };

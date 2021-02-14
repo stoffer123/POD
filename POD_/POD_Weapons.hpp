b93dc1bbf6db34c3b16211e92953435f3d2fbe20
stoffer123 <christophermikkelsen@live.dk>
2021-02-14 18:16:53 +0100

    added POD_M10_Elcan_LYLA + POD_LMG_Elcan_LYLA

--- POD_/POD_Weapons.hpp
+++ POD_/POD_Weapons.hpp
@@ -1,8 +1,10 @@
 	class gtr_weap_L119A2_CBN_Tan;
-	class POD_M10_Elcan: gtr_weap_L119A2_CBN_Tan
+	class POD_M10_Elcan_LYLA: gtr_weap_L119A2_CBN_Tan
 	{
 		author = "[POD Nebel]";
-		_generalMacro = "POD_M10_Elcan";
+		displayName="POD M10";
+		scope=2;
+		_generalMacro = "POD_M10_Elcan_LYLA";
 		magazines[] = {"30Rnd_556x45_Stanag"};
 		class LinkedItems
 		{
@@ -17,6 +19,26 @@
 				item = "rhsusf_acc_anpeq15";
 			};
 		};
-
-
+	};
+	class LMG_M60E6_F;
+	class POD_LMG_Elcan_LYLA: LMG_M60E6_F
+	{
+		author = "[POD Nebel]";
+		displayName="POD M60";
+		scope=2;
+		_generalMacro = "POD_LMG_Elcan_LYLA";
+		magazines[] = {"100Rnd_762x51_Mag"};
+		class LinkedItems
+		{
+			class LinkedItemsOptic
+			{
+				slot = "CowsSlot";
+				item = "optic_ElcanC79";
+			};
+			class LinkedItemsAcc
+			{
+				slot = "PointerSlot";
+				item = "rhsusf_acc_anpeq15";
+			};
+		};
 	};
